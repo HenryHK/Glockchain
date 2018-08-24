987ae82ad929d60016843cc3baba4c229408a59c
HenryHK <henryhan.hku@gmail.com>
2018-08-24 11:56:49 +1000

    refactor file structure

--- cli.go
+++ cli.go
@@ -101,96 +101,11 @@ func (cli *CLI) Run() {
 
 }
 
-func (cli *CLI) createBlockchain(address string) {
-	if !ValidateAddress(address) {
-		log.Panic("ERROR: Address is not valid")
-	}
-	bc := NewBlockchain(address)
-	bc.db.Close()
-	fmt.Println("Done!")
-}
-
-func (cli *CLI) printChain() {
-	cli.bc = NewBlockchain("")
-	defer cli.bc.db.Close()
-	bci := cli.bc.Iterator()
-	for {
-		block := bci.Next()
-
-		fmt.Printf("============ Block %x ============\n", block.Hash)
-		fmt.Printf("Prev. block: %x\n", block.PrevBlockHash)
-		pow := NewProofOfWork(block)
-		fmt.Printf("PoW: %s\n\n", strconv.FormatBool(pow.Validate()))
-		for _, tx := range block.Transactions {
-			fmt.Println(tx)
-		}
-		fmt.Printf("\n\n")
-
-		if len(block.PrevBlockHash) == 0 {
-			break
-		}
-	}
-}
-
 func (cli *CLI) printUsage() {
 	fmt.Println("Add Block to Blockchain: Glockchain addblock [DATA]")
 	fmt.Println("Print blockchain: Glockchain printchain")
 }
 
-func (cli *CLI) getBalance(address string) {
-	if !ValidateAddress(address) {
-		log.Panic("ERROR: Address is not valid")
-	}
-	bc := NewBlockchain(address)
-	defer bc.db.Close()
-
-	balance := 0
-	pubKeyHash := Base58Decode([]byte(address))
-	pubKeyHash = pubKeyHash[1 : len(pubKeyHash)-4]
-	UTXOs := bc.FindUTXO(pubKeyHash)
-
-	for _, out := range UTXOs {
-		balance += out.Value
-	}
-
-	fmt.Printf("Balance of '%s': %d\n", address, balance)
-}
-
-func (cli *CLI) send(from, to string, amount int) {
-	if !ValidateAddress(from) {
-		log.Panic("ERROR: Sender address is not valid")
-	}
-	if !ValidateAddress(to) {
-		log.Panic("ERROR: Recipient address is not valid")
-	}
-
-	bc := NewBlockchain(from)
-	defer bc.db.Close()
-
-	tx := NewUTXOTransaction(from, to, amount, bc)
-	bc.MineBlock([]*Transaction{tx})
-	fmt.Println("Success!")
-}
-
-func (cli *CLI) createWallet() {
-	wallets, _ := NewWallets()
-	address := wallets.CreateWallet()
-	wallets.SaveToFile()
-
-	fmt.Printf("Your new address: %s\n", address)
-}
-
-func (cli *CLI) listAddresses() {
-	wallets, err := NewWallets()
-	if err != nil {
-		log.Panic(err)
-	}
-	addresses := wallets.GetAddresses()
-
-	for _, address := range addresses {
-		fmt.Println(address)
-	}
-}
 func (cli *CLI) validateArgs() {
 	if len(os.Args) < 2 {
 		cli.printUsage()
